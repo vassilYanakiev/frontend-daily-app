2d0423f46cc50a8189282e48b727885871ff7477
Vasil Yanakiev (A1 BG) <Vasil.Yanakiev@a1.bg>
2023-12-20 08:55:35 +0200

    react-countdown package added

--- client/src/Components/ds-page/daily-pannel/TeamMemberPannelComponent.tsx
+++ client/src/Components/ds-page/daily-pannel/TeamMemberPannelComponent.tsx
@@ -1,12 +1,15 @@
 import {
+  Box,
   Button,
   Card,
   CardContent,
   CardActions,
   Typography,
 } from "@mui/material";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
 
 interface TeamMember {
+  id: string;
   name: string;
   surname: string;
   role: string;
@@ -22,20 +25,41 @@ const TeamMemberPanel: React.FC<TeamMemberPanelProps> = ({ teamMember }) => {
   return (
     <>
       <Card>
-        <CardContent>
-          <Typography variant="h5" component="div">
-            {teamMember.name} {teamMember.surname}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Role: {teamMember.role}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Availability: {teamMember.availability}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Assigned Jira Card: {teamMember.assignedJiraCard}
-          </Typography>
-        </CardContent>
+        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+          <CardContent>
+            <Typography variant="h5" component="div">
+              {teamMember.name} {teamMember.surname}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Role: {teamMember.role}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Availability: {teamMember.availability}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Assigned Jira Card: {teamMember.assignedJiraCard}
+            </Typography>
+          </CardContent>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              margin: 2,
+            }}
+          >
+            <CountdownCircleTimer
+              key={teamMember.id}
+              size={120}
+              isPlaying
+              duration={120}
+              colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+              colorsTime={[120, 30, 2, 0]}
+            >
+              {({ remainingTime }) => remainingTime}
+            </CountdownCircleTimer>
+          </Box>
+        </Box>
       </Card>
       <div style={{ display: "flex", justifyContent: "center" }}>
         <h3>Active Tickets:</h3>
